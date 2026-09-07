9ef325323dab4b4d72e332b51c629fcfca7a9cb4
sopheak <hangsopheak.hod@gmail.com>
2026-09-07 22:57:18 +0700

    s04 step 7: dynamic colour on android 12 and above

--- app/src/main/java/kh/edu/rupp/taskmanagement/ui/theme/Theme.kt
+++ app/src/main/java/kh/edu/rupp/taskmanagement/ui/theme/Theme.kt
@@ -1,11 +1,15 @@
 package kh.edu.rupp.taskmanagement.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 private val LightColors = lightColorScheme(
     primary = Blue25,
@@ -71,8 +75,18 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun TaskManagerTheme(content: @Composable () -> Unit) {
+    val darkTheme = isSystemInDarkTheme()
+    val context = LocalContext.current
+    val colorScheme = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ->
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+
     MaterialTheme(
-        colorScheme = if (isSystemInDarkTheme()) DarkColors else LightColors,
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
